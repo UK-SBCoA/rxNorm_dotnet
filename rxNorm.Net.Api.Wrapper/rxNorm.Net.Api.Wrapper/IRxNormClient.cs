afcd381f25159437200e4de9018176ec90a53be7
Smith Thay <77938144+smiththay@users.noreply.github.com>
2024-05-29 16:31:10 -0400

    modifies GetDisplayTermsAsync params

--- rxNorm.Net.Api.Wrapper/rxNorm.Net.Api.Wrapper/IRxNormClient.cs
+++ rxNorm.Net.Api.Wrapper/rxNorm.Net.Api.Wrapper/IRxNormClient.cs
@@ -7,7 +7,7 @@ namespace rxNorm.Net.Api.Wrapper
     public interface IRxNormClient
     {
         Task<string[]> FindRxCUIByStringAsync(string name, int? scopeOfSearch = null, string[] sourceLists = null, int? precision = null);
-        Task<string[]> GetDisplayTermsAsync();
+        Task<string[]> GetDisplayTermsAsync(string name);
     }
 }
 
