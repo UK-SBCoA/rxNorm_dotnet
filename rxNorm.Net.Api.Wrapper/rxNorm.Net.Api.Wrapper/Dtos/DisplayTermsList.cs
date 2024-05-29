7b85ccb2d909b65fa3ec77797258611250e1b2b3
Smith Thay <77938144+smiththay@users.noreply.github.com>
2024-05-29 16:30:13 -0400

    adds term dto

---
+++ rxNorm.Net.Api.Wrapper/rxNorm.Net.Api.Wrapper/Dtos/DisplayTermsList.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using System.Text.Json.Serialization;
+
+namespace rxNorm.Net.Api.Wrapper.Dtos
+{
+    public class DisplayTermsList
+    {
+        [JsonPropertyName("term")]
+        public List<string> Term { get; set; }
+    }
+}
